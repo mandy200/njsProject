c668b9a311dcde3989a0c0bb7e6639aeaabd911b
Amanda <32878825+mandy200@users.noreply.github.com>
2019-03-04 19:59:34 +0100

    updated products.js

--- model/product.js
+++ model/product.js
@@ -1,8 +1,9 @@
 var util = require("util");
 
 class Product {
-    constructor(name,desc,img,price,barcode,brand,colour,size,category)
+    constructor(name,desc,img,price,barcode,brand,colour,size,category,id)
     {
+        this.id=id;
         this.name = name;
         this.desc = desc;
         this.img = img;
@@ -23,9 +24,9 @@ class Product {
         this.template = "<div class=\"col-xl-4 col-sm-6\">\n" +
             "              <div class=\"product\">\n" +
             "                <div class=    \"product-image\"><img src=\"%s\" alt=\"product\" class=\"img-fluid\"/>\n" +
-            "                  <div class=\"product-hover-overlay\"><a href='/products/detail' class=\"product-hover-overlay-link\"></a>\n" +
+            "                  <div class=\"product-hover-overlay\"><a href=\"view\" class=\"product-hover-overlay-link\"></a>\n" +
             "                    <div class=\"product-hover-overlay-buttons\"><a href=\"#\" class=\"btn btn-outline-dark btn-product-left\">\n" +
-            "<i class=\"fa fa-shopping-cart\"></i></a><a href='/products/detail' class=\"btn btn-dark btn-buy\">\n" +
+            "<i class=\"fa fa-shopping-cart\"></i></a><a href=\"view/%s\" class=\"btn btn-dark btn-buy\">\n" +
             "<i class=\"fa-search fa\"></i><span class=\"btn-buy-label ml-2\">View</span></a>\n" +
             "<a href=\"#\" data-toggle=\"modal\" data-target=\"#exampleModal\" class=\"btn btn-outline-dark btn-product-right\">\n" +
             "<i class=\"fa fa-expand-arrows-alt\"></i></a>\n" +
@@ -34,14 +35,14 @@ class Product {
             "                </div>\n" +
             "                <div class=\"py-2\">\n" +
             "                  <p class=\"text-muted text-sm mb-1\">%s</p>\n" +
-            "                  <h3 class=\"h6 text-uppercase mb-1\"><a href='/products/detail' class=\"text-dark\">%s</a></h3><span class=\"text-muted\">%s</span>\n" +
+            "                  <h3 class=\"h6 text-uppercase mb-1\"><a href=\"view/%s\" class=\"text-dark\">%s</a></h3><span class=\"text-muted\">%s%s%s</span>\n" +
             "                </div>\n" +
             "              </div>\n" +
             "            </div>";
     }
     getProductView()
     {
-        return util.format(this.template,this.img,this.name,this.desc,this.price, this.brand, this.colour);
+        return util.format(this.template,this.img,this.id,this.name,this.id,this.desc,this.price, this.brand, this.colour);
     };
 
     weight() { return this.size.weight; }
